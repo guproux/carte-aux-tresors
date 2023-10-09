1c01225ecd12a51cf0ab9781f76417d5a51b3084
Guillaume PROUX-DELROUYRE <g.proux.delrouyre@gmail.com>
2023-10-09 17:06:10 +0200

    init project

---
+++ src/main/java/g/proux/App.java
@@ -0,0 +1,13 @@
+package g.proux;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
